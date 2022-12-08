645ab147c98017137dea34497eadabb5f7a63ed2
teamgram.io <wubenqi@gmail.com>
2022-12-08 10:36:50 +0800

    patch by @teamgram

--- r.sh
+++ r.sh
@@ -2,7 +2,6 @@ python3 build-system/Make/Make.py \
     --cacheDir="$HOME/telegram-bazel-cache2" \
     generateProject \
     --configurationPath=build-system/appstore-configuration.json \
-    --codesigningInformationPath=build-system/fake-codesigning \
-    --disableExtensions \
-    --disableProvisioningProfiles
+    --codesigningInformationPath="$HOME/telegram-configuration" \
+    --disableExtensions
 
